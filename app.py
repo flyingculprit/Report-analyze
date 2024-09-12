ad0194ea5ee139bbc983e6c82e414470f4b2fab8
flyingculprit <suryan2021ai@gmail.com>
2024-09-12 20:09:36 +0530

    backup

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
 # Set up pytesseract path for Windows
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
